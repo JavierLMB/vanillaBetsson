b137a791e76ce4111541a542ba2b7e9343cd3d5c
JavierLMB~ <javier.camilleri29@gmail.com>
2023-10-28 15:47:29 +0200

    "Fixing slider function"

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,57 +1,3 @@
-export function initializeCarousel() {
-  const slide = $(".slider__slide");
-  let currentIndex = 0;
-  let startX, startY;
-
-  slide.on("touchstart", function (e) {
-    const touch = e.originalEvent.touches[0];
-    startX = touch.clientX;
-    startY = touch.clientY;
-  });
-
-  slide.on("touchmove", function (e) {
-    if (!startX || !startY) {
-      return;
-    }
-
-    const touch = e.originalEvent.touches[0];
-    const deltaX = startX - touch.clientX;
-    const deltaY = startY - touch.clientY;
-
-    if (Math.abs(deltaX) > Math.abs(deltaY)) {
-      // Horizontal swipe detected
-      if (deltaX > 0) {
-        // Swipe left, show next slide
-        nextSlide();
-      } else {
-        // Swipe right, show previous slide
-        prevSlide();
-      }
-    }
-
-    // Reset start coordinates to detect next swipe
-    startX = null;
-    startY = null;
-  });
-
-  function nextSlide() {
-    currentIndex = (currentIndex + slide.length) % slide.length;
-    updateSlide();
-  }
-
-  function prevSlide() {
-    currentIndex = (currentIndex - 1 + slide.length) % slide.length;
-    updateSlide();
-  }
-
-  function updateSlide() {
-    slide.removeClass("slider__slide-active");
-    slide.eq(currentIndex).addClass("slider__slide-active");
-  }
-
-  setInterval(nextSlide, 5000);
-}
-
 // export function initializeCarousel() {
 //   const slide = $(".slider__slide");
 //   let currentIndex = 0;
@@ -81,3 +27,104 @@ export function initializeCarousel() {
 
 //   setInterval(nextSlide, 5000);
 // }
+
+// export function initializeCarousel() {
+//   const carousel = $(".slider__carousel");
+//   const slide = $(".slider__slide");
+//   const slideWidth = slide.width();
+//   let currentIndex = 0;
+
+//   function showSlide(index) {
+//     currentIndex = (index + slide.length) % slide.length;
+//     const translateValue = -slideWidth * currentIndex + "px";
+//     carousel.css({ transform: `translateX(${translateValue})` });
+//   }
+
+//   function nextSlide() {
+//     showSlide(currentIndex + 1);
+//   }
+
+//   function prevSlide() {
+//     showSlide(currentIndex - 1);
+//   }
+
+//   $(".slider__prev").click(function () {
+//     prevSlide();
+//   });
+
+//   $(".slider__next").click(function () {
+//     nextSlide();
+//   });
+
+//   setInterval(nextSlide, 5000);
+// }
+
+export function initializeCarousel() {
+  const carousel = $(".slider__carousel");
+  const slide = $(".slider__slide");
+  const slideWidth = slide.width();
+  let currentIndex = 0;
+  let isDragging = false;
+  let dragStartX = 0;
+  let dragOffset = 0;
+  let intervalId;
+
+  function showSlide(index) {
+    currentIndex = (index + slide.length) % slide.length;
+    const translateValue = -slideWidth * currentIndex + "px";
+    carousel.css({ transform: `translateX(${translateValue})` });
+  }
+
+  function nextSlide() {
+    if (!isDragging) {
+      showSlide(currentIndex + 1);
+    }
+  }
+
+  function prevSlide() {
+    if (!isDragging) {
+      showSlide(currentIndex - 1);
+    }
+  }
+
+  $(".slider__prev").click(function () {
+    prevSlide();
+  });
+
+  $(".slider__next").click(function () {
+    nextSlide();
+  });
+
+  carousel.on("mousedown touchstart", function (event) {
+    isDragging = true;
+    dragStartX = event.pageX || event.originalEvent.touches[0].pageX;
+    dragOffset = 0;
+
+    clearInterval(intervalId);
+  });
+
+  carousel.on("mousemove touchmove", function (event) {
+    if (isDragging) {
+      const dragX = event.pageX || event.originalEvent.touches[0].pageX;
+      dragOffset = dragX - dragStartX;
+      const translateValue = -slideWidth * currentIndex + dragOffset + "px";
+      carousel.css({ transform: `translateX(${translateValue})` });
+    }
+  });
+
+  carousel.on("mouseup touchend", function () {
+    isDragging = false;
+
+    if (dragOffset > 50) {
+      prevSlide();
+    } else if (dragOffset < -50) {
+      nextSlide();
+    } else {
+      showSlide(currentIndex);
+    }
+
+    intervalId = setInterval(nextSlide, 5000);
+  });
+
+  intervalId = setInterval(nextSlide, 5000);
+}
