bfc9287e90fd8a0c1eb0b0eb45e81d048ddaf188
JavierLMB~ <javier.camilleri29@gmail.com>
2023-10-28 20:44:47 +0200

    "Slider V2 Draggable"

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,130 +1,83 @@
-// export function initializeCarousel() {
-//   const slide = $(".slider__slide");
-//   let currentIndex = 0;
-
-//   function showSlide(index) {
-//     currentIndex = (index + slide.length) % slide.length;
-
-//     slide.removeClass("slider__slide-active");
-//     slide.eq(currentIndex).addClass("slider__slide-active");
-//   }
-
-//   function nextSlide() {
-//     showSlide(currentIndex + 1);
-//   }
-
-//   function prevSlide() {
-//     showSlide(currentIndex - 1);
-//   }
-
-//   $(".slider__prev").click(function () {
-//     prevSlide();
-//   });
-
-//   $(".slider__next").click(function () {
-//     nextSlide();
-//   });
-
-//   setInterval(nextSlide, 5000);
-// }
-
-// export function initializeCarousel() {
-//   const carousel = $(".slider__carousel");
-//   const slide = $(".slider__slide");
-//   const slideWidth = slide.width();
-//   let currentIndex = 0;
-
-//   function showSlide(index) {
-//     currentIndex = (index + slide.length) % slide.length;
-//     const translateValue = -slideWidth * currentIndex + "px";
-//     carousel.css({ transform: `translateX(${translateValue})` });
-//   }
-
-//   function nextSlide() {
-//     showSlide(currentIndex + 1);
-//   }
-
-//   function prevSlide() {
-//     showSlide(currentIndex - 1);
-//   }
-
-//   $(".slider__prev").click(function () {
-//     prevSlide();
-//   });
-
-//   $(".slider__next").click(function () {
-//     nextSlide();
-//   });
-
-//   setInterval(nextSlide, 5000);
-// }
-
 export function initializeCarousel() {
   const carousel = $(".slider__carousel");
   const slide = $(".slider__slide");
   const slideWidth = slide.width();
-  let currentIndex = 0;
   let isDragging = false;
   let dragStartX = 0;
   let dragOffset = 0;
   let intervalId;
+  let direction = "left";
 
-  function showSlide(index) {
-    currentIndex = (index + slide.length) % slide.length;
-    const translateValue = -slideWidth * currentIndex + "px";
-    carousel.css({ transform: `translateX(${translateValue})` });
+  function showSlide() {
+    if (direction === "right") {
+      const updatedFirstSlide = carousel.find(".slider__slide").first();
+      carousel.append(updatedFirstSlide);
+    }
+
+    if (direction === "left") {
+      const updatedLastSlide = carousel.find(".slider__slide").last();
+      carousel.prepend(updatedLastSlide);
+    }
   }
 
+  carousel.on("transitionend", function () {
+    showSlide();
+    carousel.css({ transition: "none" });
+    carousel.css({ transform: "translate(0)" });
+    setTimeout(() => carousel.css({ transition: "all 0.2s ease-in" }));
+  });
+
   function nextSlide() {
-    if (!isDragging) {
-      showSlide(currentIndex + 1);
-    }
+    direction = "right";
+    carousel.css({ transform: `translateX(${-slideWidth}px)` });
   }
 
   function prevSlide() {
-    if (!isDragging) {
-      showSlide(currentIndex - 1);
-    }
+    direction = "left";
+    carousel.css({ transform: `translateX(${slideWidth}px)` });
   }
-
-  $(".slider__prev").click(function () {
-    prevSlide();
-  });
 
   $(".slider__next").click(function () {
     nextSlide();
   });
 
+  $(".slider__prev").click(function () {
+    prevSlide();
+  });
+
   carousel.on("mousedown touchstart", function (event) {
+    event.preventDefault();
     isDragging = true;
     dragStartX = event.pageX || event.originalEvent.touches[0].pageX;
     dragOffset = 0;
-
     clearInterval(intervalId);
+    carousel.css({ transition: "none" });
   });
 
   carousel.on("mousemove touchmove", function (event) {
     if (isDragging) {
       const dragX = event.pageX || event.originalEvent.touches[0].pageX;
+      console.log(dragX);
       dragOffset = dragX - dragStartX;
-      const translateValue = -slideWidth * currentIndex + dragOffset + "px";
+      const translateValue = dragOffset + "px";
       carousel.css({ transform: `translateX(${translateValue})` });
     }
   });
 
   carousel.on("mouseup touchend", function () {
-    isDragging = false;
-
     if (dragOffset > 50) {
       prevSlide();
     } else if (dragOffset < -50) {
       nextSlide();
     } else {
-      showSlide(currentIndex);
+      carousel.css({ transform: "translate(0)" }); //fix it
     }
 
+    isDragging = false;
+
+    carousel.css({ transition: "all 0.2s ease-in" });
     intervalId = setInterval(nextSlide, 5000);
   });
 
-  intervalId = setInterval(nextSlide, 5000);
+  if (!isDragging) intervalId = setInterval(nextSlide, 5000);
 }
